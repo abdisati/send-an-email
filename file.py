01b9f9d6043ded1eab50294080340801eca2143e
Abdi Deresa <Abdishegute@gmail.com>
2024-08-26 09:36:01 -0700

    added exception handling

--- file.py
+++ file.py
@@ -14,3 +14,15 @@ Subject: {subject}\n
 
 server = smtplib.SMTP("smtp.gmail.com",587)
 server.starttls()
+
+try:
+    server.login(sender,password)
+    print("Logged in ...")
+    server.sendmail(sender, receiver, message)
+    print("Email has been sent!")
+except smtplib.SMTPAuthenticationError:
+    print("unable to sign in")
+except smtplib.SMTPServerDisconnected:
+    print("Unable to send email")
+finally:
+    server.quit()
